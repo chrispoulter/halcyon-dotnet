4272fa3b48e8e8df11c3888407a3acec4dcf38f0
Chris Poulter <cpoulter@hotmail.co.uk>
2025-06-30 16:46:54 +0100

    bugfix config conditional version check

--- Halcyon.Web/src/lib/config.ts
+++ Halcyon.Web/src/lib/config.ts
@@ -1,5 +1,6 @@
 export const config = {
     VERSION: String(import.meta.env.VERSION),
-    VITE_API_URL:
-        String(import.meta.env.VITE_API_URL) || `${window.location.origin}/api`,
+    VITE_API_URL: String(
+        import.meta.env.VITE_API_URL || `${window.location.origin}/api`
+    ),
 };
