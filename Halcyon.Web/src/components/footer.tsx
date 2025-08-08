03b828371b67c025fd94eee375ba0322d6bee7df
Chris Poulter <cpoulter@hotmail.co.uk>
2025-08-08 10:42:02 +0100

    add local date to footer

--- Halcyon.Web/src/components/footer.tsx
+++ Halcyon.Web/src/components/footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
                     {currentYear}
                 </div>
                 <div className="text-sm leading-none font-medium">
-                    v{config.VERSION}
+                    v{config.VERSION} | {new Date().toString()}
                 </div>
             </div>
         </footer>
