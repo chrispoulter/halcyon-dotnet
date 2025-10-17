661e8aad977ed195e45b1fdb00e846eeb06c2971
Chris Poulter <cpoulter@hotmail.co.uk>
2025-10-17 10:27:15 +0100

    update dependencies

--- Halcyon.Web/eslint.config.js
+++ Halcyon.Web/eslint.config.js
@@ -3,10 +3,10 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default tseslint.config([
+export default defineConfig([
     globalIgnores(['dist']),
     {
         files: ['**/*.{ts,tsx}'],
