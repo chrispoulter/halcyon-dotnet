eb76519b12b1ff5f64421538ecc96c09852e23c5
Chris Poulter <cpoulter@hotmail.co.uk>
2025-11-27 09:59:25 +0000

    add runtime value replacement

--- Halcyon.Web/src/lib/config.ts
+++ Halcyon.Web/src/lib/config.ts
@@ -1,4 +1,6 @@
 export const config = {
     VERSION: import.meta.env.VERSION,
     API_URL: `${window.location.origin}/api`,
+    VITE_RUNTIME_VALUE_1: import.meta.env.VITE_RUNTIME_VALUE_1,
+    VITE_RUNTIME_VALUE_2: import.meta.env.VITE_RUNTIME_VALUE_2,
 };
