694bb770720011c5a650b107b314053b5268bac6
Chris Poulter <cpoulter@hotmail.co.uk>
2025-12-02 14:46:49 +0000

    rollback nginx config

--- Halcyon.Web/entrypoint.sh
+++ Halcyon.Web/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Generating runtime config..."
-envsubst < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.tmp.conf
-mv /etc/nginx/conf.d/default.tmp.conf /etc/nginx/conf.d/default.conf
-
+echo "Replacing runtime environment variables..."
 envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.tmp.html
 mv /usr/share/nginx/html/index.tmp.html /usr/share/nginx/html/index.html
-
-echo "Starting Nginx... $@"
-exec "$@"
