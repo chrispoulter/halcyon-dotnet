a046c3d489f194b70fcef5a757ea98d106ead8e7
Chris Poulter <cpoulter@hotmail.co.uk>
2025-12-02 09:48:16 +0000

    manually generate nginx config to test performance on azure

--- Halcyon.Web/entrypoint.sh
+++ Halcyon.Web/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+set -e
+
 echo "Generating runtime config..."
+envsubst < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.tmp.conf
+mv /etc/nginx/conf.d/default.tmp.conf /etc/nginx/conf.d/default.conf
+
 envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.tmp.html
 mv /usr/share/nginx/html/index.tmp.html /usr/share/nginx/html/index.html
+
+echo "Starting Nginx... $@"
+exec "$@"
