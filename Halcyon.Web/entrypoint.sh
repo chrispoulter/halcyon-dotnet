740c26ea3bab5ec895421e371bbbe139a05ffac5
Chris Poulter <cpoulter@hotmail.co.uk>
2025-12-03 15:33:57 +0000

    remove nginx templating for performance

--- Halcyon.Web/entrypoint.sh
+++ Halcyon.Web/entrypoint.sh
@@ -9,3 +9,30 @@ window.__ENV__ = {
   VITE_RUNTIME_VALUE_2: "${VITE_RUNTIME_VALUE_2}",
 };
 EOF
+
+echo "Generating default.conf..."
+
+cat <<EOF > /etc/nginx/conf.d/default.conf
+server {
+    listen       ${VITE_PORT};
+    listen  [::]:${VITE_PORT};
+    server_name  localhost;
+
+    access_log  /var/log/nginx/server.access.log  main;
+
+    location / {
+        root /usr/share/nginx/html;
+        try_files \$uri \$uri/ /index.html;
+    }
+
+    location /api/ {
+        proxy_pass ${API_HTTP};
+        proxy_http_version 1.1;
+        proxy_ssl_server_name on;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        rewrite ^/api(/.*)$ \$1 break;
+    }
+}
+EOF
+
+exec "$@"
