8a2ef8a29534915876f7e4a453309b101f1f3119
Chris Poulter <cpoulter@hotmail.co.uk>
2025-12-04 10:32:11 +0000

    update readme

--- Halcyon.Web/entrypoint.sh
+++ Halcyon.Web/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Replacing runtime environment variables..."
-
 find /usr/share/nginx/html/assets -type f -name "*.js" -print0 | xargs -0 sed -i \
 	-e "s|VITE_RUNTIME_VALUE_1:void 0|VITE_RUNTIME_VALUE_1: \"${VITE_RUNTIME_VALUE_1}\"|g" \
 	-e "s|VITE_RUNTIME_VALUE_2:void 0|VITE_RUNTIME_VALUE_2: \"${VITE_RUNTIME_VALUE_2}\"|g"
