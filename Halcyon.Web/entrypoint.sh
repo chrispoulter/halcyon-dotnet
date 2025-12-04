c8372f1a519c5c7f92cf6c63871a1d2579f692f7
Chris Poulter <cpoulter@hotmail.co.uk>
2025-12-04 09:52:35 +0000

    code clean up

--- Halcyon.Web/entrypoint.sh
+++ Halcyon.Web/entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 find /usr/share/nginx/html/assets -type f -name "*.js" -print0 | xargs -0 sed -i \
-	-e "s|\__VITE_RUNTIME_VALUE_1__|${VITE_RUNTIME_VALUE_1}|g" \
-	-e "s|\__VITE_RUNTIME_VALUE_2__|${VITE_RUNTIME_VALUE_2}|g"
+	-e "s|__VITE_RUNTIME_VALUE_1__|${VITE_RUNTIME_VALUE_1}|g" \
+	-e "s|__VITE_RUNTIME_VALUE_2__|${VITE_RUNTIME_VALUE_2}|g"
