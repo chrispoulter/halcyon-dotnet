883f6a26a9d5360de738e804e5b1955562870caf
Chris Poulter <cpoulter@hotmail.co.uk>
2026-08-20 17:03:24 +0100

    refactor: format PackageReference elements and update documentation links in Extensions.cs

--- Halcyon.ServiceDefaults/Extensions.cs
+++ Halcyon.ServiceDefaults/Extensions.cs
@@ -11,9 +11,9 @@ using OpenTelemetry.Trace;
 
 namespace Microsoft.Extensions.Hosting;
 
-// Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
+// Adds common Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
-// To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
+// To learn more about using this project, see https://aka.ms/aspire/service-defaults
 public static class Extensions
 {
     private const string HealthEndpointPath = "/health";
@@ -151,9 +151,9 @@ public static class Extensions
     public static WebApplication MapDefaultEndpoints(this WebApplication app)
     {
         // Adding health checks endpoints to applications in non-development environments has security implications.
-        // See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
-        //if (app.Environment.IsDevelopment())
-        //{
+        // See https://aka.ms/aspire/healthchecks for details before enabling these endpoints in non-development environments.
+        // if (app.Environment.IsDevelopment())
+        // {
         // All health checks must pass for app to be considered ready to accept traffic after starting
         app.MapHealthChecks(HealthEndpointPath);
 
@@ -162,7 +162,7 @@ public static class Extensions
             AlivenessEndpointPath,
             new HealthCheckOptions { Predicate = r => r.Tags.Contains("live") }
         );
-        //}
+        // }
 
         return app;
     }
